db831ea1087968c6957e86b74450a0e45c254f26
Diego Bezerra <dsouzaduarte@gmail.com>
2018-12-26 20:44:15 -0200

    Make sure our styled-components styles come first

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
+    const { styleTags } = this.props
     return (
-      <html lang="en">
+      <html lang="pt-BR">
         <Head>
+          {styleTags}
           <meta name="viewport" content="width=device-width,initial-scale=1" />
           <meta name="fragment" content="!" />
           <meta name="mobile-web-app-capable" content="yes" />
