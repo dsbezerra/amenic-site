bbd3b5f543a4310e0bc10c2eb62d4062c2023d7d
Diego Bezerra <dsouzaduarte@gmail.com>
2018-12-26 20:12:28 -0200

    Coming soon

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,39 @@
-import Link from 'next/link';
+import styled from 'styled-components'
+import {
+  Box,
+  Heading,
+  Text,
+} from 'grommet'
 
-export default () => (
-  <div>
-    <h1>Welcome to a next.js docker boilerplate!</h1>
-    <Link prefetch href="/about">
-      <button>About</button>
-    </Link>
-  </div>
-);
+import withGrommet from '../lib/withGrommet'
+
+const StyledHeading = styled(Heading)`
+  letter-spacing: 8px;
+  font-feature-settings: "smcp" 1;
+  font-weight: 400;
+  font-size: 56px;
+  & span {
+    font-size: 72px;
+  }
+`
+
+const Logo = () => (
+  <StyledHeading margin="none">
+    <span>A</span>menic
+  </StyledHeading>
+)
+
+export default withGrommet(() => (
+  <Box
+    align="center"
+    justify="center"
+    fill
+  >
+    <Logo />
+    <Text
+      margin="none"
+    >
+      COMING SOON
+    </Text>
+  </Box>
+))
